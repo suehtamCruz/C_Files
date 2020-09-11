fb04c7eb1111cb7886487f63198748aacd6fdf2a
Matheus Cruz <matheuscz3110@gmail.com>
2020-09-10 21:59:01 -0300

    Conta digitos  T3

---
+++ Topico 3/Conta_Digitos.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+    int valor;
+    
+    char num[1];
+    scanf("%c %d",&num,&valor);
+
+    char valString[0];
+    //transformando o numero que foi dado para uma string para poder comparar o numero que vai estar presente com cada posição da string
+    sprintf(valString,"%d",valor); 
+    //quantidade da caracteres do numero passado    
+    int tam = strlen(valString);
+    
+    int cont=0;
+    for(int i=0;i<tam;i++){
+        if(valString[i] == num[0]){
+            cont++;
+        }    
+    }
+    
+    printf("%d\n",cont);
+
+    return 0;
+}
