a992bbac9287bf85370c6ede0ecd6af8d44381e7
Matheus Cruz <matheuscz3110@gmail.com>
2020-10-02 09:31:54 -0300

    Create pontStruc.c

---
+++ testesQuestoes/pontStruc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+struct data{
+    int hora;
+    int minuto;
+    int segundo;
+};
+
+
+int main(void){
+    struct data hoje,*pHoje;
+    pHoje = &hoje;
+
+    pHoje -> hora = 20;
+    pHoje -> minuto = 39;
+    pHoje -> segundo = 22;
+
+    int *pHora = &hoje.minuto;
+    printf("%d",*pHora);
+    printf("%d : %d : %d",hoje.hora , hoje.minuto , hoje.segundo);
+    return 0;
+}
